8eae43d56e169163501232b256761b525a90a634
Nikhil Yadav <facts.foundr@gmail.com>
2025-01-24 14:56:21 +0530

    modified header.jsx

--- app/chatPage/_components/Header.jsx
+++ app/chatPage/_components/Header.jsx
@@ -3,6 +3,7 @@ import React, { useEffect,useState } from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import ThemeToggleButton from './ThemeToggleButton'
+import { Button } from "@/components/ui/button";
 
 function Header() {
   const pathname = usePathname(); //usePathname hook tell the active bath.
@@ -12,7 +13,7 @@ function Header() {
   }, [pathname]);
 
   return (
-    <div className='max-w-[80vw] m-auto '>
+    <div className='max-w-[80vw] m-auto py-2 border rounded-lg mt-2'>
       <div className=" flex text-xl justify-between p-3 md:p-0 md:px-8 items-center" >
 
         <Link href="/"  className=' text-xl font-medium md:font-bold cursor-pointer'>
@@ -21,7 +22,7 @@ function Header() {
 
         <div className='flex justify-between gap-[12vw] items-center  '>
               <ul className='hidden md:flex  justify-between items-center p-2 gap-10'>
-                {/* <Link href='/chatPage' className='text-[1vw] font-medium cursor-pointer hover:font-bold   transition-all'>Chat</Link> */}
+                <Link href='/chatPage' className='text-[1vw] font-medium cursor-pointer hover:font-bold   transition-all'>Chat</Link>
                 <Link href="/about" className='text-[1vw] font-medium cursor-pointer hover:font-bold  transition-all'>About</Link>
                 <Link href="/contact" className='text-[1vw] font-medium cursor-pointer hover:font-bold  transition-all'>Contact</Link>
                 <Link href="/FAQs" className='text-[1vw] font-medium cursor-pointer hover:font-bold  transition-all'>FAQs</Link>
@@ -29,9 +30,9 @@ function Header() {
 
               <ThemeToggleButton/>
               
-              <div className='hidden md:block  text-[1vw] font-medium cursor-pointer '>
-                Signup
-              </div>
+              <Link href='#' className="outline-none" >
+                 <Button >Signup</Button>
+             </Link>
         </div>
          
       </div>
--- app/page.js
+++ app/page.js
@@ -8,22 +8,22 @@ export default function Home() {
       <div  className=" max-w-[80vw] m-auto ">
       <div className="  w-full  flex flex-col justify-center  items-center">
 
-            <div className=' flex flex-col justify-center items-center p-4   mt-[18vh]'>
+            <div className=' flex flex-col justify-center items-center p-4   mt-[14vh]'>
                   <h2  className="  font-semibold tracking-tight py-4 text-6xl">Redefine Your Mental Wellness</h2>
-                  <div className=" text-center mx-20 px-10 leading-tight tracking-tight text-slate-500 text-[1.4vw]">
+                  <div className=" text-center mx-24 px-10 leading-tight  text-gray-500 text-md">
                       Unlock mental clarity with our AI-Psychologist. Share your thoughts, receive personalized guidance, 
                       and find support anytime, anywhere.
                       Your journey to better well-being starts here.
                   </div>
             </div>
 
-            <Link href='/chatPage' >
-                 <Button className='my-5 mb-20'>Try for free</Button>
+            <Link href='/chatPage' className="outline-none" >
+                 <Button >Try for free</Button>
              </Link>
 
             
-              <Image src={"/heroImg.png"} width={1000} height={800} alt="heroImg"
-              className="border  rounded-md shadow-lg text-center bg-"/>
+              <Image src={"/heroImg2.png"} width={1100} height={800} alt="heroImg"
+              className="rounded-xl mt-16 "/>
 
 
        </div>
